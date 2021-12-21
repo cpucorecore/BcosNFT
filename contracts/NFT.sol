f43f427acba6b81954511b45bb1fbe75f79575b3
刘坤瑜 <sky@liukunyudeMacBook-Pro.local>
2021-12-21 15:49:16 +0800

    update sol contracts

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -2,16 +2,16 @@ pragma solidity >=0.6.0 <0.8.0;
 
 import "./ERC721.sol";
 
-contract Wines is ERC721 {
+contract NFT is ERC721 {
     constructor(string memory name, string memory symbol) ERC721(name, symbol) public {}
 
-    function issueWineNFT(address to, uint256 tokenId, string memory tokenURI) public
+    function mintNFT(address to, uint256 tokenId, string memory tokenURI) public
     {
         _mint(to, tokenId);
         _setTokenURI(tokenId, tokenURI);
     }
 
-    function burnWineNFT(uint256 tokenId) public
+    function burnNFT(uint256 tokenId) public
     {
         _burn(tokenId);
     }
